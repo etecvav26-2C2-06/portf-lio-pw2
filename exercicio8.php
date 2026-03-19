7e15638523e080af03c26bc1afc7506b603a2ce9
Duquesjf <pedrodjsjf@gmail.com>
2026-03-18 21:13:21 -0300

    Add files via upload

--- exercicio8.php
+++ exercicio8.php
@@ -1,3 +1,29 @@
+<!-- 
+ Data: 18/03/2026
+ Autor: Pedro Menezes Duque José
+Objetivo: 
+Exercício 8 - Soma de 0 até N com Função
+Faça um programa que receba um número inteiro, determinando a soma de 0 até tal número (incluso) por meio de uma função soma(n) que retorne tal soma.
+*/
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+
+
+
+
 <?php
 
  
