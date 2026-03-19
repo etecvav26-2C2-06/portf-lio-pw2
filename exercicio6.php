649330f5bf653d34be6c704d10464a11bc3a3399
Gabriel Macedo <gabrielmaartins75@gmail.com>
2026-03-19 02:42:55 -0300

    atualização exercicio6.php

--- exercicio6.php
+++ exercicio6.php
@@ -1,3 +1,16 @@
+<!-- 
+ Data: 18/03/2026
+ Autor: Nícolas de Oliveira
+Objetivo: ler um número e mostrar os termos da sequencia de fibonacci
+Exercício 6 - Série de Fibonacci
+Leia um número n e mostre os n primeiros termos da sequência de Fibonacci.
+
+Exemplo:
+n = 12
+Resultado:
+0 1 1 2 3 5 8 13 21 34 55 89
+-->
+
 <?php
 
 $n = (int)readline("Quantos termos de Fibonacci você deseja ver? ");
