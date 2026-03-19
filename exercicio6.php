ab6bb286e3899370241ae58ed6012145aea77d62
Nícolas Oliveira <reizinn2009@gmail.com>
2026-03-19 02:21:31 -0300

    Update exercicio6.php

--- exercicio6.php
+++ exercicio6.php
@@ -9,3 +9,13 @@ $resultado = [];
 for ($i = 0; $i < $n; $i++) {
     //armazenamos o termo atual na nossa lista de resultados
     $resultado[] = $atual;
+    
+//lógica da sequência:
+    $soma = $atual + $proximo;
+    $atual = $proximo;
+    $proximo = $soma;
+}
+
+echo "Resultado: " . implode(" ", $resultado) . PHP_EOL;
+
+?>
